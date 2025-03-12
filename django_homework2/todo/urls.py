f4640824c4897969a960dfb7b4345f04077a2c41
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-03-12 17:55:02 +0900

    Delete django_homework2 directory

--- django_homework2/todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from todo.views import todo_list, todo_info
-
-urlpatterns = [
-    path('', todo_list, name='todo_list'), # 전체 목록 페이지
-    path('<int:todo_id>/', todo_info, name='todo_info') # 개별 상세 페이지
-]
