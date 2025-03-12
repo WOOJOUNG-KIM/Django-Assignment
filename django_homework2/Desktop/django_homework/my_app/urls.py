3d99e42afb667ecd30150a63711dd56835176e12
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-03-12 17:46:11 +0900

    Delete django_homework2 directory

--- django_homework2/Desktop/django_homework/my_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views # 같은 폴더에 있는 views.py 파일에서 함수 가져오기
-
-urlpatterns = [
-    path('', views.user_list, name='user_list'), # /users/ → user_list() 함수 실행
-    path('<int:user_id>/', views.user_info, name='user_info'), # /users/1/ → user_info() 함수 실행
-]
