56e46c688d6f9d56d0480a256b7829916227d1e7
WOOJOUNG-KIM <dnwnd660@daum.net>
2025-03-12 12:08:42 +0900

    chapte02 재업로드 과제 .gitignore 추가

---
+++ django_homework2/Desktop/oz_django/django_homework1/config/urls.py
@@ -0,0 +1,24 @@
+"""
+URL configuration for config project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include # include를 가져와야 여러 앱을 관리할 수 있음!
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include('my_app.urls')), # ''users/' 요청을 my_app.urls에서 처리!
+]
+
